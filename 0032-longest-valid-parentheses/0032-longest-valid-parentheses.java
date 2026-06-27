99f998c48039d0ed7ca651a511683e1d5cfde118
Honey Chauhan <209209172+yHoney-72@users.noreply.github.com>
2026-06-27 23:46:52 +0530

    Time: 8 ms (5.05%), Space: 46.6 MB (26.36%) - LeetHub

--- 0032-longest-valid-parentheses/0032-longest-valid-parentheses.java
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.java
@@ -1,31 +1,20 @@
 class Solution {
     public int longestValidParentheses(String s) {
-        int left = 0 , right = 0 , maxLen = 0;
-     for(int i = 0 ; i<s.length(); i++){
+       Stack<Integer>stack = new Stack<>();
+       int max = 0 ;
+       stack.push(-1);
+       for(int i = 0 ; i<s.length(); i++){
         if(s.charAt(i)=='('){
-            left++;
+            stack.push(i);
         }else{
-            right++;
+            stack.pop();
         }
-        if(left==right){
-            maxLen = Math.max(maxLen,2*left);
-        }else if(left<right){
-            left = 0 ; right = 0;
-        }
-     }
-     left = right =0; 
-     for(int i = s.length()-1;i>=0; i--){
-        if(s.charAt(i)=='('){
-            left ++;
+        if(stack.isEmpty()){
+            stack.push(i);
         }else{
-            right++;
+            max = Math.max(max,i-stack.peek());
         }
-        if(left ==right){
-            maxLen = Math.max(maxLen,2*left);
-        }else if(left>right){
-            left = 0 ; right = 0;
-        }
-     }
-     return maxLen;
+       }
+       return max;
     }
 }
