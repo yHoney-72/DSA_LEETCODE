ead970152e0341165f6e9590ac0b0d69b847fe09
Honey Chauhan <209209172+yHoney-72@users.noreply.github.com>
2026-06-30 22:24:50 +0530

    Time: 2 ms (90.14%), Space: 46.6 MB (86.76%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -10,23 +10,38 @@
  */
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-        ListNode dummy = new ListNode(-1);
-        ListNode tail = dummy;
-        PriorityQueue<ListNode>pq = new PriorityQueue<>((a,b)->a.val-b.val);
-        for(ListNode head : lists){
-            if(head!=null){
-                pq.offer(head);
-            }
-        }
-        while(!pq.isEmpty()){
-          ListNode next = pq.poll();
-          tail.next = next;
-            tail = tail.next;
-            if(next.next!=null){
-                pq.offer(next.next);
-            }
-        }
-        return dummy.next;
-        }
-        
+      if(lists == null || lists.length==0){
+        return null ;
+      }
+      return mergeLists(lists , 0 , lists.length-1);
     }
+    private ListNode merge(ListNode list1 , ListNode list2){
+      ListNode dummy = new ListNode(-1);
+      ListNode tail = dummy;
+      while(list1!=null && list2!= null){
+        if(list1.val<=list2.val){
+            tail.next = list1;
+            list1 = list1.next;
+        }else{
+            tail.next = list2;
+            list2 = list2.next;
+        }
+        tail = tail.next;
+      }
+      if(list1!=null){
+        tail.next = list1;
+      }else{
+        tail.next = list2;
+      }
+      return dummy.next;
+    }
+    private ListNode mergeLists(ListNode[] lists, int left, int right){
+        if(left == right){
+            return lists[left] ;
+        }
+        int mid = left+(right - left)/2;
+        ListNode leftNode = mergeLists(lists,left , mid);
+        ListNode rightNode = mergeLists(lists,mid+1, right);
+       return merge(leftNode , rightNode);
+    }
+}
