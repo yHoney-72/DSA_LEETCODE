99d7aa2887cd6c1f115119d4a8ca2cc128229417
Honey Chauhan <209209172+yHoney-72@users.noreply.github.com>
2026-07-04 11:00:02 +0530

    Time: 18 ms (81.87%), Space: 98 MB (65.28%) - LeetHub

--- 0077-combinations/0077-combinations.java
+++ 0077-combinations/0077-combinations.java
@@ -2,24 +2,18 @@ class Solution {
     public List<List<Integer>> combine(int n, int k) {
         List<List<Integer>>result = new ArrayList<>();
         List<Integer>current = new ArrayList<>();
-        boolean[]used = new boolean[n+1];
-        callFun(result,n , k , current,used , 1);
+        callFun(result,n , k , current , 1);
         return result;
     }
-    private void callFun(List<List<Integer>>result,int n,int k,List<Integer>current,boolean[] used,int start){
+    private void callFun(List<List<Integer>>result,int n,int k,List<Integer>current,int start){
         if(current.size()==k){
              result.add(new ArrayList<>(current));
              return;
         }
        for(int i = start ; i<=n;i++){
-        if(used[i]==true){
-            continue;
-        }
         current.add(i);
-         used[i] = true;
-         callFun(result , n , k , current,used,i+1);
+         callFun(result , n , k , current,i+1);
          current.remove(current.size() - 1); 
-          used[i] = false;
        }
         }
     }
