0027b65f317db78a7be65d7a5791eadb5db2e17d
Honey Chauhan <209209172+yHoney-72@users.noreply.github.com>
2026-07-15 17:42:29 +0530

    Time: 0 ms (100%), Space: 45.9 MB (9.97%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.java
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -15,24 +15,23 @@
  */
 class Solution {
     public boolean isBalanced(TreeNode root) {
+       return height(root)!=-2;
+    }
+    private int height(TreeNode root){
         if(root==null){
-            return true;
+            return -1;
         }
-        int left = height(root.left);
-        int right = height(root.right);
-        if(Math.abs(left-right)>1){
-            return false;
+        int leftHeight =  height(root.left);
+        int rightHeight = height(root.right);
+        if(leftHeight==-2){ // height 0 1 se +ve number hota -2 leliye as a checker vas ye bf n hai -2 ki jagh -1 chod ke koi bi (-int) lelo vas -1 ko chod 0 and +ve int chod
+            return -2;
         }
-        return isBalanced(root.left)&&isBalanced(root.right);
+        if(rightHeight==-2){
+            return -2;
+        }
+        if(Math.abs(leftHeight-rightHeight)>1){
+            return -2;
+        }
+        return 1+Math.max(leftHeight,rightHeight);
     }
-   private int height(TreeNode root){
-    if(root== null){
-        return -1;
-    }
-    int leftHeight = height(root.left);
-    int righHeight = height(root.right);
-    int rootHeight = 1+Math.max(leftHeight,righHeight);
-    return rootHeight;
-   }
-   
 }
