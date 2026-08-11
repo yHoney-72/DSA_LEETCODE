9d011083c9ec12321652db0c8384f056c4a1ccad
Honey Chauhan <209209172+yHoney-72@users.noreply.github.com>
2026-08-11 21:51:22 +0530

    Time: 0 ms (100%), Space: 48.4 MB (11.75%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,5 +1,20 @@
 class Solution {
-    public double myPow(double x, int n) {
-        return Math.pow(x,n);
+    public double myPow(double x, int N) {
+        long n = N; //for last test cas kyuki overflow ho rha 
+        double ans = 1.0;
+        if(n<0){
+            x=1/x;
+            n=-n;
+        }
+        while(n>0){
+           if(n%2!=0){
+            ans*=x;
+            n--;
+           }else{
+            x*=x;
+            n/=2;
+           }
+        }
+        return ans;
     }
 }
