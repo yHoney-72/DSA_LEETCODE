742bfc468523888843528a71c234de6140e17b23
Honey Chauhan <209209172+yHoney-72@users.noreply.github.com>
2026-08-25 02:46:55 +0530

    Time: 156 ms (27.99%), Space: 42.8 MB (75.64%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,45 +1,43 @@
 class Solution {
-    static int dir[][] = {{-1,0},{1,0},{0,-1},{0,1}};
+    static int dirs[][] = {{-1,0},{1,0},{0,-1},{0,1}};
     public boolean exist(char[][] board, String word) {
-        int n = board.length;
-        int m = board[0].length;
-        char arr[] = word.toCharArray();
-        int k = 0;
-        boolean[][] visited = new boolean[board.length][board[0].length];
-        for(int i = 0; i<n;i++){
-            for(int j = 0 ;j<m;j++){
-                if(board[i][j]==arr[k]){
-                   visited[i][j] = true;
-                boolean found=  helper(k+1,i,j,board,arr,visited);
-                visited[i][j] = false;
-                if(found){
-                    return true;
-                }
-                }
+      int n = board.length;
+      int m = board[0].length;
+      boolean visited[][] = new boolean[n][m];
+      char arr[] = word.toCharArray();
+      int k =0;
+      for(int i = 0 ; i<n; i++){
+        for(int j = 0 ;j<m; j++){
+            if(arr[k]==board[i][j]){
+               visited[i][j] = true;
+               boolean found = helper(k+1,i,j,board,arr,visited);
+               visited[i][j] = false;
+               if(found){
+                 return true;
+               }
             }
         }
-        return false;
+      }
+      return false;
     }
-    private boolean helper(int k , int i , int j,char board[][],char arr[],boolean visited[][]){
+    private boolean helper(int k ,int i , int j , char[][]board,char[]arr,boolean[][]visited){
         if(k==arr.length){
             return true;
         }
-        for(int x[]:dir){
-          int newRow = x[0]+i;
-          int newCol = x[1]+j;
-         if(newRow>=0&&newCol>=0&&newRow<board.length&& newCol<board[0].length&&visited[newRow][newCol]==false){
-            if(board[newRow][newCol]!=arr[k]){
-                continue;
-            }else{
-                visited[newRow][newCol] = true;
-               boolean found =  helper(k+1,newRow,newCol,board,arr,visited);
-               visited[newRow][newCol] = false;
-               if(found){
-                return true;
+        for(int dir[]:dirs){
+            int newRow = dir[0]+i;
+            int newCol = dir[1]+j;
+           if(newRow>=0&&newRow<board.length && newCol>=0&&newCol<board[0].length&&visited[newRow][newCol]==false){
+               if(board[newRow][newCol]==arr[k]){
+                visited[newRow][newCol]=true;
+                boolean found = helper(k+1,newRow, newCol,board,arr,visited);
+                 visited[newRow][newCol]=false;
+                 if(found){
+                      return true;
+                 }
                }
-            }
-            }
-         }
-         return false;
+           }
         }
-    }
+        return false;
+    }        
+}
