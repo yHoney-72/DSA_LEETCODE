973d82089fd5c0730d610aac41931b3d23dabdd7
Honey Chauhan <209209172+yHoney-72@users.noreply.github.com>
2026-09-22 19:07:26 +0530

    Time: 1 ms (100%), Space: 48.9 MB (32.31%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,20 +1,34 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int n = nums1.length;
-        int m = nums2.length;
-       int arr[] = new int[n+m];
-       for (int i = 0; i < n; i++) {
-            arr[i] = nums1[i];
+    if(nums1.length>nums2.length){
+        return findMedianSortedArrays(nums2,nums1);
+    }
+    int n = nums1.length;
+    int m = nums2.length;
+    int low =0, high = n;
+    int half = (n+m+1)/2;
+    while(low<=high){
+        int cut1 = (low+high)/2;
+        int cut2 = half -cut1;
+        int left1 = (cut1==0)?Integer.MIN_VALUE:nums1[cut1-1];
+        int right1 = (cut1==n)?Integer.MAX_VALUE:nums1[cut1];
+        int left2 =  (cut2==0)?Integer.MIN_VALUE:nums2[cut2-1];
+        int right2 = (cut2==m)?Integer.MAX_VALUE:nums2[cut2];
+        if(left1<=right2&&left2<=right1){
+            if((n+m)%2==1){
+                return Math.max(left1,left2);
+            }else{
+                int a = Math.max(left1,left2);
+                int b = Math.min(right1,right2);
+                return (a+b)/2.0;
+            }
         }
-       for (int i = 0; i < m; i++) {
-            arr[n + i] = nums2[i];
+        if(left1>right2){
+           high = cut1-1;
+        }else if(left2>right1){
+            low = cut1+1;
         }
-       int z = arr.length;
-       Arrays.sort(arr);
-       if(z%2==0){
-        return  (arr[z/2]+arr[z/2-1])/2.0;
-       }else{
-        return  arr[(z-1)/2];
-       }
+     }
+     return 0.0;
     }
 }
