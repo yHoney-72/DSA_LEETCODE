9213b13ba0bbbf2813baa0467ed3ed3f80f77706
Honey Chauhan <209209172+yHoney-72@users.noreply.github.com>
2026-09-24 21:39:50 +0530

    Time: 1 ms (21.25%), Space: 48.7 MB (7.04%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.java
+++ 0344-reverse-string/0344-reverse-string.java
@@ -1,12 +1,14 @@
 class Solution {
     public void reverseString(char[] s) {
-        int left =0, right =s.length-1;
-        while(left<right){
-            char temp = s[left];
-            s[left]=s[right];
-            s[right] = temp;
-            left++;
-            right--;
+        helper(0,s.length-1,s);
+    }
+    private void helper(int left, int right,char[]s){
+        if(left>=right){
+            return;
         }
+        char temp = s[left];
+        s[left] = s[right];
+        s[right] = temp;
+        helper(left+1,right-1,s);//++left --right use kaor par left++ right -- n kyuki ye smae valeu pass kar deta hai 
     }
 }
